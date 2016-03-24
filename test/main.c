9730644e0b36e89a7bc14d32b0cef1a180bc1ab5
Don Goodman-Wilson <don@slack-corp.com>
2016-03-23 17:53:25 -0700

    Write tests that will pass on OSX

--- test/main.c
+++ test/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#ifndef __APPLE__
 #include "openssl/md5.h"
 #include "openssl/crypto.h"
+#endif
  
 int main(void)
 {
   CURL *curl;
   CURLcode res;
+  int retval = 0;
  
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com/");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://httpbin.org/get");
     res = curl_easy_perform(curl);
  
     if(CURLE_OK == res) {
@@ -18,15 +21,23 @@ int main(void)
       /* ask for the content-type */ 
       res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
  
-      if((CURLE_OK == res) && ct)
+      if((CURLE_OK == res) && ct) {
         printf("We received Content-Type: %s\n", ct);
+      } else {
+        printf("No content-type\n");
+        retval = 1;
+      }
+    } else {
+      printf("Request failed\n");
+      retval = 2;
     }
  
     /* always cleanup */ 
     curl_easy_cleanup(curl);
+  } else {
+    printf("Failed to init curl\n");
+    retval = 3;
   }
   
-  printf("SSL library version: %s\n", SSLeay_version(SSLEAY_VERSION));
-  
-  return 0;
+  return retval;
 }
