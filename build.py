8d2970dd5ffa214eff44637f735ca9b5e16b42f5
Luis <lasote@gmail.com>
2016-03-30 10:08:37 +0200

    Additional builds for macos

--- build.py
+++ build.py
@@ -11,4 +11,14 @@ if __name__ == "__main__":
                 static_builds.append([build[0], build[1]])
 
         builder.builds = static_builds
+        for compiler in builder.apple_clang_versions:
+            builder.add({"compiler": "apple-clang", "compiler.version": compiler, 
+                         "arch": "x86_64", "build_type": "Release"}, {"libcurl:shared": False,
+                                                                  "libcurl:darwin_ssl": False,
+                                                                  "libcurl:custom_cacert": True})
+            builder.add({"compiler": "apple-clang", "compiler.version": compiler, 
+                         "arch": "x86_64", "build_type": "Debug"}, {"libcurl:shared": False,
+                                                                  "libcurl:darwin_ssl": False,
+                                                                  "libcurl:custom_cacert": True})
+        
     builder.run()
