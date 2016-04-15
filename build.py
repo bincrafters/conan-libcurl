a2aad925d68aa45b807c8131dcf27bcd325914ef
Lasote <lasote@gmail.com>
2016-04-15 13:07:38 +0200

    Try with shared

--- build.py
+++ build.py
@@ -3,14 +3,20 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True)
+    builder.add_common_builds(pure_c=True, shared_option_name="libcurl:shared")
+    accepted_builds = []
+    if platform.system() == "Linux": 
+        for build in builder.builds:
+            if build[0]["arch"] != "x86":
+                accepted_builds.append([build[0], build[1]])
+        builder.builds = accepted_builds
+
     if platform.system() == "Darwin": 
-        static_builds = []
         for build in builder.builds:
             if not build[0]["arch"] == "x86":
-                static_builds.append([build[0], build[1]])
+                accepted_builds.append([build[0], build[1]])
 
-        builder.builds = static_builds
+        builder.builds = accepted_builds
         for compiler in builder.apple_clang_versions:
             builder.add({"compiler": "apple-clang", "compiler.version": compiler, 
                          "arch": "x86_64", "build_type": "Release"}, {"libcurl:shared": False,
@@ -19,6 +25,5 @@ if __name__ == "__main__":
             builder.add({"compiler": "apple-clang", "compiler.version": compiler, 
                          "arch": "x86_64", "build_type": "Debug"}, {"libcurl:shared": False,
                                                                   "libcurl:darwin_ssl": False,
-                                                                  "libcurl:custom_cacert": True})
-        
+                                                           "libcurl:custom_cacert": True})
     builder.run()
