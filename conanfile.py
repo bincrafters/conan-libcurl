60670af3f2495d06b29f692093b4b48f4405c974
theirix <theirix@gmail.com>
2017-11-26 18:34:24 +0300

    Returned option with_largemaxwritesize

--- conanfile.py
+++ conanfile.py
@@ -19,11 +19,12 @@ class LibcurlConan(ConanFile):
                "with_libssh2": [True, False],
                "with_libidn": [True, False],
                "with_librtmp": [True, False],
-               "with_libmetalink": [True, False]}
+               "with_libmetalink": [True, False],
+               "with_largemaxwritesize": [True, False]}
     default_options = "shared=False", "with_openssl=True", "disable_threads=False", \
                       "with_ldap=False", "custom_cacert=False", "darwin_ssl=True",  \
                       "with_libssh2=False", "with_libidn=False", "with_librtmp=False", \
-                      "with_libmetalink=False"
+                      "with_libmetalink=False", "with_largemaxwritesize=False"
     exports = ["FindCURL.cmake"]
     url = "http://github.com/bincrafters/conan-libcurl"
     license = "https://curl.haxx.se/docs/copyright.html"
@@ -65,6 +66,10 @@ class LibcurlConan(ConanFile):
             self.run("chmod +x ./%s/configure" % self.name)
 
     def build(self):
+        if self.options.with_largemaxwritesize:
+            tools.replace_in_file(os.path.join(self.name, 'include', 'curl', 'curl.h'),
+                                  "define CURL_MAX_WRITE_SIZE 16384", "define CURL_MAX_WRITE_SIZE 10485760")
+
         if self.settings.os == "Linux" or self.settings.os == "Macos":
 
             suffix = " --without-libidn " if not self.options.with_libidn else "--with-libidn"
