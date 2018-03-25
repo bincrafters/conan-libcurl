2b72cee34b348328474b7767399372f4819e492d
theirix <theirix@gmail.com>
2018-03-25 13:01:35 +0300

    Enhanced example to dump protocols and versions

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -9,7 +9,17 @@ int main(void)
 {
   CURL *curl;
   int retval = 0;
- 
+  const char *const *proto;
+  curl_version_info_data* id = curl_version_info(CURLVERSION_NOW);
+  if (!id)
+    return 1;
+
+  printf("protocols: ");
+  for(proto = id->protocols; *proto; proto++) {
+    printf("%s ", *proto);
+  }
+  printf("\nversion: %s\nssl version: %s\nfeatures: %d\n", id->version, id->ssl_version, id->features);
+
   curl = curl_easy_init();
   if(curl) {
     char errbuf[CURL_ERROR_SIZE];
@@ -19,10 +29,11 @@ int main(void)
 
     /* always cleanup */ 
     curl_easy_cleanup(curl);
+    printf("Succeed\n");
   } else {
     printf("Failed to init curl\n");
     retval = 3;
   }
-  
+
   return retval;
 }
