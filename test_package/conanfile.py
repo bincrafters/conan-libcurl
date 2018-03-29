366c70768275d916767bbcb4980b7ebe50462040
Uilian Ries <uilianries@gmail.com>
2018-03-29 19:45:20 -0300

    Build ARM on CI
    - To valide ARM support, a dedicated job was add on Travis CI
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,6 +3,8 @@
 
 from conans import ConanFile, CMake, tools, RunEnvironment
 import os
+import subprocess
+import re
 
 
 class TestPackageConan(ConanFile):
@@ -22,4 +24,13 @@ class TestPackageConan(ConanFile):
             elif self.settings.os == "Macos":
                 self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+                if "arm" in self.settings.arch:
+                    self.test_arm()
+                else:
+                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+
+    def test_arm(self):
+        file_ext = "so.4.5.0" if self.options["libcurl"].shared else "a"
+        lib_path = os.path.join(self.deps_cpp_info["libcurl"].libdirs[0], "libcurl.%s" % file_ext)
+        output = subprocess.check_output(["readelf", "-h", lib_path]).decode()
+        assert re.search(r"Machine:\s+ARM", output)
